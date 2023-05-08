fb5bf7a0c47c6e8385eaf7a478a8ae962248d25f
sahiljarsaniya <77919760+sahiljarsaniya@users.noreply.github.com>
2023-05-08 22:53:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+function App() {
+  return (
+    <AuthWrapper>
+    <Router>
+      <Routes>
+       <Route path='/' element={
+       <PrivateRoute>
+         <Dashboard/>
+       </PrivateRoute>
+       }/>
+       <Route path='login' element={<Login/>}/>
+       <Route path='*' element={<Error/>}/>
+      </Routes>
+    </Router>
+    </AuthWrapper>
+  );
+}
+
+export default App;
